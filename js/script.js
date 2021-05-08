1a6d96e32bab63b280934bbdb034e6851cdb412a
sachiyo sugaya <sachiyology@gmail.com>
2021-05-09 03:40:09 +0900

    updated

---
+++ js/script.js
@@ -0,0 +1,12 @@
+//task 1.0
+const mainEL = document.querySelector('main');
+//task1.1
+mainEL.style.backgroundColor = 'var(--main-bg)';
+//task1.2
+mainEL.innerHTML = '<h1>SEI Rocks!</h1>';
+//task1.3
+mainEl.classList.add('flex-ctr');
+// 2.0
+const topMenuEl = document.querySelector('top-menu');
+// 2.1
+topMenuEl.style.height = 100;
