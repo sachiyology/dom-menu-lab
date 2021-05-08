0e9dbc76d2c424500a52c2140775ba487e3961dd
sachiyo sugaya <sachiyology@gmail.com>
2021-05-09 04:38:36 +0900

    pushing all the updates

--- js/script.js
+++ js/script.js
@@ -1,12 +1,34 @@
+// 3.0
+// Menu data structure
+var menuLinks = [
+  {text: 'about', href: '/about'},
+  {text: 'catalog', href: '/catalog'},
+  {text: 'orders', href: '/orders'},
+  {text: 'account', href: '/account'},
+];
+
 //task 1.0
-const mainEL = document.querySelector('main');
+const mainEl = document.querySelector('main');
 //task1.1
-mainEL.style.backgroundColor = 'var(--main-bg)';
+mainEl.style.backgroundColor = 'var(--main-bg)';
 //task1.2
-mainEL.innerHTML = '<h1>SEI Rocks!</h1>';
+mainEl.innerHTML = '<h1>SEI Rocks!</h1>';
 //task1.3
 mainEl.classList.add('flex-ctr');
 // 2.0
-const topMenuEl = document.querySelector('top-menu');
+const topMenuEl = document.getElementById('top-menu');
 // 2.1
-topMenuEl.style.height = 100;
+topMenuEl.style.height = "100%";
+// 2.2
+topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
+//2.3
+topMenuEl.classList.add('flex-around');
+
+//3.1
+for (let link of menuLinks){
+  const linkTag = document.createElement('a');
+  linkTag.seetAttribute('href', link.href);
+  linkTag.innerHTML = link.text;
+  topMenuEl.append(linkTag);
+  console.log(LinkTag)
+}
