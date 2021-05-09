cccdf08139a6bbcec105cf7c201a9612ee82ce49
sachiyo sugaya <sachiyology@gmail.com>
2021-05-10 04:00:19 +0900

    push the last update

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
 // 3.0
+// 5.0
 // Menu data structure
 var menuLinks = [
   {text: 'about', href: '/about'},
-  {text: 'catalog', href: '/catalog'},
-  {text: 'orders', href: '/orders'},
-  {text: 'account', href: '/account'},
+  {text: 'catalog', href: '#', subLinks: [
+    {text: 'all', href: '/catalog/all'},
+    {text: 'top selling', href: '/catalog/top'},
+    {text: 'search', href: '/catalog/search'},
+  ]},
+  {text: 'orders', href: '#' , subLinks: [
+    {text: 'new', href: '/orders/new'},
+    {text: 'pending', href: '/orders/pending'},
+    {text: 'history', href: '/orders/history'},
+  ]},
+  {text: 'account', href: '#', subLinks: [
+    {text: 'profile', href: '/account/profile'},
+    {text: 'sign out', href: '/account/signout'},
+  ]},
 ];
 
 //task 1.0
@@ -27,8 +39,56 @@ topMenuEl.classList.add('flex-around');
 //3.1
 for (let link of menuLinks){
   const linkTag = document.createElement('a');
-  linkTag.seetAttribute('href', link.href);
+  linkTag.setAttribute('href', link.href);
   linkTag.innerHTML = link.text;
+  // linkTag.classList.add('active');
   topMenuEl.append(linkTag);
-  console.log(LinkTag)
 }
+
+//4.0
+const subMenuEl = document.getElementById('sub-menu');
+
+//4.1
+subMenuEl.style.height = "100%";
+//4.2
+subMenuEl.style.backgroundColor = 'var(--sub-menu-bg)';
+
+//4.3
+subMenuEl.classList.add ('flex-around');
+
+//4.4
+subMenuEl.style.position = 'absolute';
+
+//4.5
+subMenuEl.style.top = '0';
+
+//5.1
+const topMenuLinks = document.querySelectorAll('#top-menu a')
+let showingSubMenu = false;
+
+//5.2
+topMenuEl.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  const link = evt.target
+  if (link.tagName != 'A') {
+    return;
+  }else{
+    console.log(link.textContent)
+  }
+
+//5.3
+const checker = link.classLisst.contains('active')
+console.log(checker)
+
+if (checker){
+  link.classList.remove("active");
+  showingSubMenu = false;
+  subMenuEl.style.top = '0';
+  return;
+}
+if (!checker){
+  link.classList.add('active')
+}
+});
+//5.6
+showingSubMenu = true
